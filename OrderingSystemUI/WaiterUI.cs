e2a105827cea2db53c23dd4b8b7c0f28fd324815
jojunicorn <684123@student.inholland.nl>
2022-05-23 14:49:45 +0200

    waiterUI

---
+++ OrderingSystemUI/WaiterUI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OrderingSystemUI
+{
+    public partial class WaiterUI : Form
+    {
+        public WaiterUI()
+        {
+            InitializeComponent();
+        }
+
+        private void WaiterUI_Load(object sender, EventArgs e)
+        {
+            //Disables tabs in tabcontrol
+            tabControl.Appearance = TabAppearance.FlatButtons;
+            tabControl.ItemSize = new Size(0, 1);
+            tabControl.SizeMode = TabSizeMode.Fixed;
+        }
+    }
+}
