795c76bf40b5ec4cbe5ddbb9963298f8432da8a2
margariitag <margarita.glushaka@gmail.com>
2022-05-31 13:20:44 +0200

    payment ui

---
+++ OderingSystemLogic/PaymentService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OrderingSystemModel;
+using OrderingSystemDAL;
+
+namespace OrderingSystemLogic
+{
+    public class PaymentService
+    {
+        private PaymentDAO paymentDao;
+
+        
+    }
+}
---
+++ OrderingSystemDAL/PaymentDAO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using OrderingSystemModel;
+namespace OrderingSystemDAL
+{
+    public class PaymentDAO :BaseDAO
+    {
+       
+        
+    }
+}
