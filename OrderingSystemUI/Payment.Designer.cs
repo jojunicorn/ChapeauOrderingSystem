f28aa5b917a436113a7538115eca23cb2f808ee1
margariitag <margarita.glushaka@gmail.com>
2022-06-01 08:44:14 +0200

    testing commit

--- OrderingSystemUI/Payment.Designer.cs
+++ OrderingSystemUI/Payment.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Payment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(750, 450);
+            this.Name = "Payment";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
