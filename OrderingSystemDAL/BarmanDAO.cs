bdb69a0f68fd1c8b3fcac7c266b3444dad47515b
MariaRalucaDiaconescu <90312191+MariaRalucaDiaconescu@users.noreply.github.com>
2022-06-03 15:33:44 +0200

    Added BarmanDAO

---
+++ OrderingSystemDAL/BarmanDAO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OrderingSystemModel;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace OrderingSystemDAL
+{
+    public class BarmanDAO : BaseDAO
+    {
+        public List<OrderProduct> GetAllOrders()
+        {
+            string query = "SELECT R.OrderNumber, R.ProductId, R.OrderTime, P.ProductName FROM [dbo].[ORDERPRODUCTS] AS R JOIN [dbo].[PRODUCT] AS P ON R.ProductId = P.ProductId ORDER BY R.OrderNumber";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+
+        private List<OrderProduct> ReadTables(DataTable dataTable)
+        {
+            List<OrderProduct> orderProducts = new List<OrderProduct>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                OrderProduct orderProduct = new OrderProduct()
+                {
+                    ItemID = (int)dr["ItemId"],
+                    OrderNumber = (int)dr["OrderNumber"],
+                    ProductID = (int)dr["TableNumber"],
+                    Comment = (string)dr["Comment"],
+                    OrderTime = (DateTime)dr["OrderTime"],
+                };
+                orderProducts.Add(orderProduct);
+            }
+
+            return orderProducts;
+        }
+
+    }
+}
